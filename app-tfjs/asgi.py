c5c6a182ec318e819ee798d2e0bf2a2706f3b21b
neoaman <amanthe001@gmail.com>
2021-05-30 19:36:32 +0530

    mod: modified the project name and Readme File

--- app-tfjs/asgi.py
+++ app-tfjs/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for control project.
+ASGI config for app-tfjs project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- app-tfjs/wsgi.py
+++ app-tfjs/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for control project.
+WSGI config for app-tfjs project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'control.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app-tfjs.settings')
 
 application = get_wsgi_application()
